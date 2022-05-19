fe9ef58dad553c58dbe1616304d11e16bf2dd3e5
Monte DuMond <gdumond42@gmail.com>
2022-05-19 19:56:33 -0400

    Ariplane level 3

--- Airplane/master.js
+++ Airplane/master.js
@@ -1,3 +1,4 @@
+// VARIABLES
 var playersDiv = document.getElementById('players'),
     playerDict = {topVal: 620, leftVal:410, type:"player"},
     enemiesDiv = document.getElementById('enemies'),
@@ -7,7 +8,19 @@ var playersDiv = document.getElementById('players'),
         {topVal: 60, leftVal:410, type: "enemy"},
         {topVal: 40, leftVal:530, type: "enemy"},
         {topVal: 20, leftVal:650, type: "enemy"},
-    ]
+    ],
+    missileDict = [];
+
+
+
+// FUNCTIONS
+function drawMissile() {
+    content = '';
+    for(i=0;i<missileDict.length;i++){
+        content += '<div class="missile" style="top:'+(missileDict[i].topVal)+'px; left:'+(missileDict[i].leftVal)+'px;"></div>'
+    }
+    document.getElementById('missiles').innerHTML = content;
+}
 
 function drawCharacter(character){
     content = "<div class='character "+character.type+"' style='top:"+character.topVal+"px; left:"+character.leftVal+"px;'></div>";
@@ -22,6 +35,15 @@ function drawEnemies(){
     enemiesDiv.innerHTML = content;
 }
 
+function moveObjects(objDict, moveAmount){
+    for(i=0;i<objDict.length;i++){
+        objDict[i].topVal += moveAmount;
+    }
+}
+
+
+
+// PLAYERS ACTIONS
 document.onkeydown = function (e){
     if(e.keyCode == 40 && playerDict.topVal<620){ // down
             playerDict.topVal+=5;
@@ -35,17 +57,28 @@ document.onkeydown = function (e){
     } if(e.keyCode == 39 && playerDict.leftVal<840){ // right
             playerDict.leftVal+=5;
             playersDiv.innerHTML = drawCharacter(playerDict);
-    } if(e.keyCode == 32){
-
+    } if(e.keyCode == 32){ // fire
+        if(missileDict.length%2==0){
+            missileDict.push({topVal: playerDict.topVal-5, leftVal:playerDict.leftVal+34});
+        } else {
+            missileDict.push({topVal: playerDict.topVal-5, leftVal:playerDict.leftVal+21});
+        }        
+        drawMissile();
     }
 }
 
-playersDiv.innerHTML = drawCharacter(playerDict);
-drawEnemies();
 
-setInterval(function(){
-    for(i=0;i<enemiesDict.length;i++){
-        enemiesDict[i].topVal +=2;
-    }
+
+// GAME LOOP
+function gameLoop(){
+    playersDiv.innerHTML = drawCharacter(playerDict);
+
+    moveObjects(enemiesDict, 1);
     drawEnemies();
-},30)
+
+    drawMissile();
+    moveObjects(missileDict,-16)
+
+    setTimeout(gameLoop, 50)
+}
+gameLoop();
